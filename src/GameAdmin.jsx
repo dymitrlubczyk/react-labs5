f17aa9189e14d1cb0af2d52907761aec01b4b433
dymitrlubczyk <dymitrlubczyk@gmail.com>
2019-11-09 17:49:50 +0100

    Lab5Ex3

--- src/GameAdmin.jsx
+++ src/GameAdmin.jsx
@@ -8,18 +8,41 @@ class GameAdmin extends React.Component {
             user1Name: '',
             user2Name: '',
             user1Score: 0,
-            user2Score: 0
+            user2Score: 0,
+            user1Label:'user is playing now',
+            user2Label:'play'
 
         }
     }
 
+    changePlayer=(playerNr)=>{
+        
 
+        if(playerNr=='1')
+        {
+            this.setState({
+                user1Label:'user is playing now',
+                user2Label:'play'
+            })
+        }
+
+        else
+        {
+            this.setState({
+                user1Label:'play',
+                user2Label:'user is playing now'
+            })
+        }
+    }
 
     render() {
        return ( 
            <>
-           <Player playerNr='1' playerName={this.state.user1Name}/>
-           <Player playerNr='2' playerName={this.state.user2Name}/>
+           <Player playerNr='1' playerName={this.state.user1Name} 
+           changePlayer={this.changePlayer} label={this.state.user1Label}/>
+           
+           <Player playerNr='2' playerName={this.state.user2Name} 
+           changePlayer={this.changePlayer} label={this.state.user2Label}/>
            
                 <input
                     type='text'
--- src/Player.jsx
+++ src/Player.jsx
@@ -5,18 +5,27 @@ class Player extends React.Component {
     constructor(props){
         super(props);
         this.state={
-            playerName: props.playerName,
             playerNr: props.playerNr,
-            played: 0
+            played: 0,
         };
 
     }
+
+    clickHandler=()=>{
+
+        this.props.changePlayer(this.state.playerNr);
+    }
+    
     render(){
         return(
             <div>
         <div>Player{this.state.playerNr}</div>
+        
         <div>Name {this.props.playerName}</div>
-        <div>Played number of times: {this.state.played}<button>button</button></div>
+        
+        <div>Played number of times: {this.state.played}
+        <button onClick={this.clickHandler} disabled={!this.label=='play'}>{this.props.label}</button>
+        </div>
         </div>)
         
     }
